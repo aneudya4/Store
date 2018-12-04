55b224999fb8dff94eb578c19ba4202ed35e90d7
Aneudy Adames <a4aneudy@gmail.com>
2018-12-04 00:47:21 -0500

    Updates

---
+++ precache-manifest.ac856df5d0c6a20f76443b2cb4d84c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a4c619b26b8c8ffb308b729454f24f3",
+    "url": "/aneudya4/Store/static/media/mainbanner.9a4c619b.jpg"
+  },
+  {
+    "revision": "eea567af428e598da12a",
+    "url": "/aneudya4/Store/static/js/runtime~main.eea567af.js"
+  },
+  {
+    "revision": "7353b4729321fb78447c",
+    "url": "/aneudya4/Store/static/js/main.7353b472.chunk.js"
+  },
+  {
+    "revision": "48f5204c7325e1e08869",
+    "url": "/aneudya4/Store/static/js/1.48f5204c.chunk.js"
+  },
+  {
+    "revision": "7353b4729321fb78447c",
+    "url": "/aneudya4/Store/static/css/main.76e5f24f.chunk.css"
+  },
+  {
+    "revision": "cc1d322e4ce23b79524d0e875bd5f44e",
+    "url": "/aneudya4/Store/index.html"
+  }
+];
