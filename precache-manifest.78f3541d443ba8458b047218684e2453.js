3314a1f8f232c1f47db34f6743152ee708c05e41
Aneudy Adames <a4aneudy@gmail.com>
2018-12-04 22:20:25 -0500

    Updates

--- precache-manifest.78f3541d443ba8458b047218684e2453.js
+++ precache-manifest.78f3541d443ba8458b047218684e2453.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/Store/static/js/runtime~main.4f3202bd.js"
   },
   {
-    "revision": "8648e495118d17a95b00",
-    "url": "/Store/static/js/main.8648e495.chunk.js"
+    "revision": "706ec035e4bfb9ccbcf1",
+    "url": "/Store/static/js/main.706ec035.chunk.js"
   },
   {
     "revision": "48f5204c7325e1e08869",
     "url": "/Store/static/js/1.48f5204c.chunk.js"
   },
   {
-    "revision": "8648e495118d17a95b00",
+    "revision": "706ec035e4bfb9ccbcf1",
     "url": "/Store/static/css/main.443eb4c5.chunk.css"
   },
   {
-    "revision": "ffc920b65d713cf4db9db52eec08fe96",
+    "revision": "e60ca7409670f4a53f05d4ecf378cd92",
     "url": "/Store/index.html"
   }
 ];
