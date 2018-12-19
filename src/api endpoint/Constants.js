316ce45d3eed2db8e79b08a48ac008b10cfb46d0
Aneudy Adames <a4aneudy@gmail.com>
2018-12-19 00:22:06 -0500

    ignored folder with api key for privacy

--- src/api endpoint/Constants.js
+++ src/api endpoint/Constants.js
@@ -1,4 +1,4 @@
-export const phonesItems =
-  "https://api.bestbuy.com/beta/products/mostViewed(categoryId=pcmcat305200050000)?apiKey=zOyps5iN0ySoVtDgXtubx35V";
-export const phonesAccesories =
-  "https://api.bestbuy.com/beta/products/mostViewed(categoryId=abcat0811006)?apiKey=zOyps5iN0ySoVtDgXtubx35V";
+import { key } from "../apiConfig/apiConfig";
+
+export const phonesItems = `https://api.bestbuy.com/beta/products/mostViewed(categoryId=pcmcat305200050000)?apiKey=${key}`;
+export const phonesAccesories = `https://api.bestbuy.com/beta/products/mostViewed(categoryId=abcat0811006)?apiKey=${key}`;
