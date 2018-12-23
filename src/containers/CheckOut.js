7f30d85094379245d7b0412db0c34f9ae8d02e42
Aneudy Adames <a4aneudy@gmail.com>
2018-12-23 00:54:56 -0500

    added proptype validation to the checkout component

--- src/containers/CheckOut.js
+++ src/containers/CheckOut.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const CheckOut = ({ total }) => {
   return (
@@ -10,6 +11,10 @@ const CheckOut = ({ total }) => {
       </h3>
     </div>
   );
+};
+
+CheckOut.propTypes = {
+  total: PropTypes.number
 };
 
 export default CheckOut;
