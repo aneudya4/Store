eb36501c3092876a5a3c0d2f0aea579faab90e0e
Aneudy Adames <a4aneudy@gmail.com>
2019-01-22 23:32:44 -0500

    changed api categoryID to display more items

--- src/api endpoint/Constants.js
+++ src/api endpoint/Constants.js
@@ -1,4 +1,7 @@
 import { key } from "../apiConfig/apiConfig";
 
-export const phonesItems = `https://api.bestbuy.com/beta/products/mostViewed(categoryId=pcmcat305200050000)?apiKey=${key}`;
+export const phonesItems = `https://api.bestbuy.com/beta/products/mostViewed(categoryId=pcmcat158500050014)?apiKey=${key}`;
 export const phonesAccesories = `https://api.bestbuy.com/beta/products/mostViewed(categoryId=abcat0811006)?apiKey=${key}`;
+
+// newCatID =[pcmcat158500050014]
+// oldCatID=[pcmcat305200050000]
